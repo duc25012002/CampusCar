4de0d91a47933523ca3a8b22e2c0b77c47701ba9
Ngô Trần Hoàng <tranhoang@joco.vn>
2023-12-22 23:26:31 +0700

    Development|Pull code

--- lib/auth/controller/LoginController.dart
+++ lib/auth/controller/LoginController.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: prefer_interpolation_to_compose_strings
 
+import 'dart:convert';
+
 import 'package:campus_car_joco/api/ApiDefine.dart';
 import 'package:campus_car_joco/api/Server.dart';
 import 'package:campus_car_joco/models/AuthLogin.dart';
 import 'package:campus_car_joco/models/LoginModel.dart';
-import 'package:campus_car_joco/routes/Routes.dart';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -28,22 +30,31 @@ class LoginController extends GetxController {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('tokenApi');
 
+    // String jsonBody = json.encode(body);
     var response = await server.postRequest(
         endPoint: Api.login +
             "username=${authLogin.userName}" +
             '&' +
             "password=${authLogin.userPass}",
         token: token);
+    // print(response.statusCode);
 
     if (response != null && response.statusCode == 200) {
       LoginModel? loginModel;
       loginModel = loginModelFromJson(response.body);
+
+      //print(loginModel.id);
       if (loginModel.code == -1) {
         customSnackbar("Fail", "Login Failed", Colors.red);
       } else {
         controllerUserName.clear();
         controllerUserPass.clear();
-        Get.offAllNamed(Routes.home);
+        Map body = {
+          'username': authLogin.userName,
+          'password': authLogin.userPass,
+          'isLogined': '1',
+        };
+        prefs.setString("AccountInstant", jsonEncode(body));
         customSnackbar("Successful", "Welcome Back!", Colors.green);
       }
     }
