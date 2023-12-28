88213751cf2a8679c83e1bb7f30cf0fd8034ed77
Hoang Duc <ducth@minhhoangjsc.com>
2023-12-28 09:27:05 +0700

    CC-11|Hoàn thành Reponsive màn Login

--- lib/components/MainInput.dart
+++ lib/components/MainInput.dart
@@ -1,20 +1,29 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first, must_be_immutable
-import 'package:campus_car_joco/utils/Colors.dart';
 import 'package:flutter/material.dart';
 
+import 'package:campus_car_joco/utils/Colors.dart';
 
-class MainInput extends StatelessWidget {
+class MainInput extends StatefulWidget {
   TextEditingController controller;
   String? hint;
   IconData? icon;
+  bool obscureText;
+  bool isPassword;
 
   MainInput({
     Key? key,
     required this.controller,
-    required this.icon,
     this.hint,
+    required this.icon,
+    required this.obscureText,
+    required this.isPassword,
   }) : super(key: key);
 
+  @override
+  State<MainInput> createState() => _MainInputState();
+}
+
+class _MainInputState extends State<MainInput> {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -23,19 +32,43 @@ class MainInput extends StatelessWidget {
         elevation: 2.0,
         borderRadius: const BorderRadius.all(Radius.circular(30)),
         child: TextField(
-          controller: controller,
+          controller: widget.controller,
           cursorColor: ColorConst.blue3,
-          obscureText: true,
+          obscureText: widget.obscureText,
           decoration: InputDecoration(
-              hintText: hint,
+              hintText: widget.hint,
               prefixIcon: Material(
                 elevation: 0,
                 borderRadius: const BorderRadius.all(Radius.circular(30)),
                 child: Icon(
-                  icon,
+                  widget.icon,
                   color: ColorConst.primaryColor,
                 ),
               ),
+              suffixIcon: widget.isPassword == true
+                  ? InkWell(
+                      splashColor: Colors.transparent,
+                      hoverColor: Colors.transparent,
+                      focusColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      onTap: () {
+                        setState(() {
+                          widget.obscureText = !widget.obscureText;
+                        });
+                      },
+                      child: Material(
+                        elevation: 0,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(30)),
+                        child: Icon(
+                          widget.obscureText
+                              ? Icons.remove_red_eye
+                              : Icons.remove_red_eye_outlined,
+                          color: ColorConst.primaryColor,
+                        ),
+                      ),
+                    )
+                  : null,
               border: InputBorder.none,
               contentPadding:
                   const EdgeInsets.symmetric(horizontal: 25, vertical: 13)),
